a57b1cc25910eb261154ebf31ea880b51ef285cf
LocoCat <superlazyzazzy@gmail.com>
2023-02-19 15:14:42 +0300

    add files for test shop

--- conftest.py
+++ conftest.py
@@ -1,49 +1,20 @@
-from selenium import webdriver
 import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-# для корректного отображения кириллицы в параметризаторах
-def pytest_make_parametrize_id(config, val): return repr(val)
-
-
-# добавляем параметр запуска тестов в командной строке(чем запускать, хромом или фаерфоксом) По умолчанию хром 
-def pytest_addoption(parser):
-    # parser.addoption('--browser_name', action='store', default=None, help="Choose browser: chrome or firefox")
-    # Можно задать значение параметра по умолчанию, 
-    # чтобы в командной строке не обязательно было указывать параметр --browser_name, например, так:
-    parser.addoption('--browser_name', action='store', default="chrome", help="Choose browser: chrome or firefox")
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: '--browser_name=chrome' or '--browser_name=firefox'")
-
     parser.addoption('--language', action='store', default=None,
-                     help="Choose language: '--language=en' or '--language=ru'")
-
-def r_browser(request):
-    browser_name = request.config.getoption("browser_name")
-    user_language = request.config.getoption("language")
-
-    options = Options()
-    options.add_experimental_option(
-        'prefs', {'intl.accept_languages': user_language})
-
-    options_firefox = OptionsFirefox()
-    options_firefox.set_preference("intl.accept_languages", user_language)
+                     help='Choose language')
 
 
-# Запуск браузера(для каждой функции)
-@pytest.fixture(scope="function")  # по умолчанию запускается для каждой функции
+@pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")  # получаем параметр командной строки browser_name
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart Сhrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart Firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    user_language = request.config.getoption('language')
+    print("\nstart chrome browser for test..")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
