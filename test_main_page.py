d8f66ad36bd34456c3fd0267d4ee6be91172806d
Konstantin <122589520+LokoCat@users.noreply.github.com>
2023-02-19 16:45:38 +0300

    File with test cases for main page_1

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,41 @@
-link = "http://selenium1py.pythonanywhere.com/"
+import pytest
 
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
-def test_guest_can_go_to_login_page(browser): 
-   browser.get(link) 
-   go_to_login_page(browser) 
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+
+    def test_guest_can_go_to_login_page(self,browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()                      # открываем страницу
+        page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+def test_should_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser , link)
+    page.open()
+    page.should_be_login_form()
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/"
+    page = BasketPage(browser,link)
+    page.open()
+    page.go_to_basket_page()
+    page.should_be_message_empty_basket()
+
+@pytest.mark.skip
+def test_guest_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page.should_not_be_empty_basket()
